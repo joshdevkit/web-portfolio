c801739da8e17f2f4516921d31049126bbfbda98
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2025-02-17 19:26:54 +0800

    added projects

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["assets.aceternity.com", "ui.aceternity.com"],
+    domains: [
+      "assets.aceternity.com",
+      "ui.aceternity.com",
+      "images.unsplash.com",
+    ],
   },
 };
 
