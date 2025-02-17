151ca4cc9d1afe5bee40eb3bddc5b67b0d0864d6
Joshua Pacho <102531192+joshdevkit@users.noreply.github.com>
2025-02-17 19:34:14 +0800

    fix nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,42 +1,39 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 import { ModeToggle } from "./ui/mode-toggle";
+import {
+  CodeXmlIcon,
+  ContainerIcon,
+  HomeIcon,
+  InfoIcon,
+  PhoneIcon,
+} from "lucide-react";
 
 const FloatingNavbar = () => {
   return (
     <header
       className="fixed top-3 left-1/2 transform -translate-x-1/2 bg-white/20 backdrop-blur-md shadow-lg rounded-lg 
-      px-4 h-10 flex items-center space-x-4 z-50"
+      px-4 h-12 flex items-center space-x-6 z-50"
     >
-      <a
-        href="#home"
-        className="text-neutral-800 dark:text-white font-semibold hover:text-neutral-200 dark:hover:text-neutral-900 transition"
-      >
-        Home
-      </a>
-      <a
-        href="#about"
-        className="text-neutral-800 dark:text-white font-semibold hover:text-neutral-200 dark:hover:text-neutral-900 transition"
-      >
-        About
-      </a>
-      <a
-        href="#services"
-        className="text-neutral-800 dark:text-white font-semibold hover:text-neutral-200 dark:hover:text-neutral-900 transition"
-      >
-        Services
-      </a>
-      <a
-        href="#projects"
-        className="text-neutral-800 dark:text-white font-semibold hover:text-neutral-200 dark:hover:text-neutral-900 transition"
-      >
-        Projects
-      </a>
-      <a
-        href="#contacts"
-        className="text-neutral-800 dark:text-white font-semibold hover:text-neutral-200 dark:hover:text-neutral-900 transition"
-      >
-        Contact
-      </a>
+      {[
+        { href: "#home", icon: <HomeIcon />, key: "home" },
+        { href: "#about", icon: <InfoIcon />, key: "about" },
+        { href: "#services", icon: <CodeXmlIcon />, key: "services" },
+        { href: "#projects", icon: <ContainerIcon />, key: "projects" },
+        { href: "#contacts", icon: <PhoneIcon />, key: "contacts" },
+      ].map(({ href, icon, key }) => (
+        <motion.a
+          key={key}
+          href={href}
+          whileHover={{ scale: 2 }}
+          transition={{ type: "spring", stiffness: 250 }}
+          className="text-neutral-800 dark:text-white font-semibold transition"
+        >
+          {icon}
+        </motion.a>
+      ))}
       <ModeToggle />
     </header>
   );
